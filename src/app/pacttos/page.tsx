572482507f4c6ce0d54066ceeda90456ef7489d2
ydroulis <lombardi.droulis@gmail.com>
2024-08-13 19:53:06 -0300

    feature: controlled input

--- src/app/pacttos/page.tsx
+++ src/app/pacttos/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 import SearchInput from '../components/SearchInput';
 
 const Pacttos: React.FC = () => {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e: { target: { value: string } }) => {
+    const newSearchValue = e.target.value;
+    setSearch(newSearchValue);
+  };
+
   return (
     <S.Main>
       <S.Title>
@@ -12,12 +19,7 @@ const Pacttos: React.FC = () => {
       </S.Title>
 
       <S.NotFound>No Pacttos found</S.NotFound>
-      <SearchInput
-        search={''}
-        handleChange={function (e: { target: { value: string } }): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
+      <SearchInput search={search} handleChange={handleChange} />
     </S.Main>
   );
 };
